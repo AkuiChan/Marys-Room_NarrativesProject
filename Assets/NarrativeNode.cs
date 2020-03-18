0214ac72f770e750bcc29937b4c6d65b63739a67
MitchFly <mfly16@student.aau.dk>
2020-03-18 13:16:47 +0100

    Initial commit

---
+++ Assets/NarrativeNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct line
+{
+    [TextArea(2, 5)]
+    public string text;
+}
+
+[CreateAssetMenu(fileName =("Element"),menuName =("Scriptable/NarrativeNodes"))]
+
+public class NarrativeNode : ScriptableObject
+{
+    public string character;
+    public line[] lines;
+}
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody rb;
+    CharacterController cc;
+
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (cc.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        cc.Move(moveDirection * Time.deltaTime);
+    }
+}
